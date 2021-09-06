ecdefe7ec1610d6e034390b75e493df7929fe7b3
Tendai <tendai1@hotmail.com>
2021-09-06 17:18:39 +0200

    set tele head

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Missions = () => (
   <container>
-  <table>
-    <thead></thead>
-  </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>{' '}</th>
+        </tr>
+      </thead>
+      <tbody />
+    </table>
   </container>
-  
+
 );
 
 export default Missions;
