87205e9716bd923b10148f40984ed8c25053fd15
Tendai <tendai1@hotmail.com>
2021-09-07 17:35:29 +0200

    Add table for profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Container, Table } from 'react-bootstrap';
 
-const Profile = () => <div>Profile page under construction</div>;
+const Profile = () => (
+  <div>
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>My Missions</th>
+            <th>My Rockets</th>
+            <th>{'  '}</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr>
+            <td><p>Telstar</p></td>
+            <td><p>Falcon 9</p></td>
+            <td />
+          </tr>
+          <tr>
+            <td><p>SeS</p></td>
+            <td><p>Falcon Heavy</p></td>
+            <td />
+          </tr>
+          <tr>
+            <td><p>AsiaSat</p></td>
+            <td><p>missionDescription</p></td>
+            <td />
+          </tr>
+          <tr>
+            <td><p>Telstar</p></td>
+            <td><p>missionDescription</p></td>
+            <td />
+          </tr>
+
+        </tbody>
+      </Table>
+    </Container>
+  </div>
+);
 
 export default Profile;
