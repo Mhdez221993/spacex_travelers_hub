15405cbcb58fce93346ae773fe911aaa53f813f6
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-07 16:04:08 +0200

    Add Dragon function

--- src/components/Dragons/Dragons.js
+++ src/components/Dragons/Dragons.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import store from '../redux/configureStore';
 
-const Dragons = () => <div>Dragons page under construction</div>;
 
-export default Dragons;
+function Dragons(props) {
+ const [dragonsDisplay, setDragonsDisplay] = useState(null);
+ const [calledDragon, setCalledDragon] = useState(null);
+
+ useEffect(() => {
+   const { dragons } = props;
+   if (!calledDragon && dragons.length === 0) {
+     setCalledDragon(true);
+     props.getDragons();
+   }
+   if (dragons !== undefined && dragons !== dragonsDisplay) {
+     setDragonsDisplay(dragons);
+   }
+ });
